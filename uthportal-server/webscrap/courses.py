e9793c89c1482424ec825a968dca6439c3637eb3
Dimitrios Paraschas <paraschas@gmail.com>
2013-12-24 08:44:08 +0200

    a couple of changes; the testbench doesn't work

--- uthportal-server/webscrap/courses.py
+++ uthportal-server/webscrap/courses.py
@@ -1,73 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from BeautifulSoup import BeautifulSoup, Tag
+# this module contains functions used to scrap pages of courses residing on
+# the server at inf-server.inf.utf.gr
+
 import requests
+from bs4 import BeautifulSoup, Tag
 
-# UTH Server courses URL #
-coursesLink = 'http://inf-server.inf.uth.gr/courses/'
+# UTH courses server URL
+courses_link = 'http://inf-server.inf.uth.gr/courses/'
 
-def GetTree(link):
+def get_soup(link):
     """
-    Returns the BeautifulSoup HTML Tree
+    get the HTML of the page, create and return the BeautifulSoup object
     """
 
-    # Download the webpage #
-    webpage = requests.get( coursesLink + 'CE120/' )
+    # download the webpage
+    webpage = requests.get(courses_link + 'CE120/')
 
-    # Parse the HTML code into DOM using BeautifulSoup #
-    tree = BeautifulSoup( webpage.content );
+    # fix encoding issue
+    webpage.encoding = 'utf-8'
 
-    # Return the tree #
-    return tree
+    # create a BeautifulSoup object
+    soup = BeautifulSoup(webpage.content)
+
+    # return the soup
+    return soup
+
 
 def CE120():
     """
-    COURSE     :    CE120 -> Προγραμματισμός Ι
-    HTML FORMAT:    <div class="announce">
-                        <p>
-                        <span class="date"> #date1# </span> #announce1#
-                        </p>
-                        <p>
-                        <span class="date"> #date2# </span> #announce2#
-                        </p>
-                        ...
-                    </div>
-    RETURN     :    List of tuples: [ (date1,announce1) , ... ]
+    course: CE120 : Προγραμματισμός 1
+
+    HTML format:
+    <div class="announce">
+        <p>
+        <span class="date"> #date1# </span> #announce1#
+        </p>
+        <p>
+        <span class="date"> #date2# </span> #announce2#
+        </p>
+        ...
+    </div>
+
+    return:
+    list of tuples: [(date1, announce1) , ...]
     """
 
-    # Get the HTML tree #
-    tree = GetTree(coursesLink + 'CE120/')
+    # get the soup
+    soup = get_soup(courses_link + 'CE120/')
 
-    # Find announce class #
-    # tree.find( tag_name , attributes ) #
-    announce_class = tree.find('div', attrs = {'class':'announce'} )
+    # find the class announce
+    # soup.find(tag_name, attributes)
+    announce_class = soup.find('div', attrs = {'class':'announce'})
 
-    # Parse the dates and create the list #
-    date_elements = announce_class.findAll('span', attrs = {'class':'date'} );
-    date_list = [ date.text[:-1] for date in date_elements ]
+    # parse the dates and create the list
+    date_elements = announce_class.findAll('span', attrs = {'class':'date'})
+    date_list = [date.text[:-1] for date in date_elements]
 
-    # Parse the announcements and create the list #
-    text_elements = announce_class.findAll('p');
+    # parse the announcements and create the list
+    text_elements = announce_class.findAll('p')
     text_list = list()
 
     for element in text_elements:
         if len(element.text) > 0:
             text = unicode()
             for block in element.contents[2:]:
-                if type(block) == Tag: # Tag -> BeautifulSoup.Tag #
+                if type(block) == Tag: # Tag -> BeautifulSoup.Tag
                     text += block.text
                 else:
                     text += block
 
-            text_list.append( text.strip() )
+            text_list.append(text.strip())
 
-    # Return the tuples #
-    return [ (date,text) for (date,text) in zip( date_list, text_list) ]
+    # return the tuples
+    return [(date, text) for (date, text) in zip(date_list, text_list)]
 
 
-## Testbench ##
-#announcement = CE120();
-#for (date,text) in announcement:
-#    print date
-#    print text + '\n'
+### testbench
+announcement = CE120()
+for (date, text) in announcement:
+    print(date)
+    print(text + '\n')
