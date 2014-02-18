384f82a0a7e2a93b82c09e8aee032ca996452cbb
Konstantinos Kanellis <kkanelli@hotmail.com>
2014-02-18 21:50:34 +0200

    Minor changes in runserver.py - Better error handling - Fixed bad parameters in flask.jsonify

--- server/runserver.py
+++ server/runserver.py
@@ -7,6 +7,9 @@ app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = True
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+HTTPCODE_NOT_FOUND = 404
+HTTPCODE_NOT_IMPLEMENTED = 501
+
 client = MongoClient()
 
 # Overide class for JSONEncoder
@@ -14,12 +17,12 @@ class BSONEncoderEx(JSONEncoder):
     def default(self, obj, **kwargs):
         from bson.objectid import ObjectId
         from datetime import datetime
-        
+
         if isinstance(obj, ObjectId):
             return str(obj)
         if isinstance(obj, datetime):
             return obj.isoformat()
-        else:            
+        else:
             return JSONEncoder.default(self, obj, **kwargs)
 
 app.json_encoder = BSONEncoderEx
@@ -28,7 +31,7 @@ app.json_encoder = BSONEncoderEx
 def show_courses():
     db_courses = client.uthportal.inf.courses.find()
     courses = { doc['code']:doc for doc in db_courses }
-    return flask.jsonify(**courses)
+    return flask.jsonify(courses)
 
 
 @app.route('/inf/courses/<course_name>')
@@ -37,21 +40,26 @@ def show_course(course_name):
     if isinstance(db_doc, dict):
         return flask.jsonify(db_doc)
     else:
-        flask.abort(404)
-        
+        flask.abort(HTTPCODE_NOT_FOUND)
+
 @app.route('/uth/food-menu')
 def show_food_menu():
     db_doc = client.uthportal.uth.food_menu.find_one({'name':'food_menu'})
     if isinstance(db_doc, dict):
-        return flask.jsonify(**db_doc)
+        return flask.jsonify(db_doc)
     else:
-        flask.abort
+        flask.abort(HTTPCODE_NOT_IMPLEMENTED)
 
-@app.errorhandler(404)
+def json_error(code, message):
+    return flask.jsonify( {'error': {'code': code, 'message': message} } ), code
+
+@app.errorhandler(HTTPCODE_NOT_FOUND)
 def page_not_found(error):
-    
-    return flask.jsonify( { 'error': { 'code' : 404, 'message': 'Page not found' } } )
-    
+    return json_error(HTTPCODE_NOT_FOUND,'Page not Found')
+
+@app.errorhandler(HTTPCODE_NOT_IMPLEMENTED)
+def not_implemented(error):
+    return json_error(HTTPCODE_NOT_IMPLEMENTED,'Not implemented')
 
 if __name__ == '__main__':
     app.run(debug=True)
