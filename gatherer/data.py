5f4cb39eef5793bea8182edcc91fe2e2b0a07248
Konstantinos Kanellis <kkanelli@hotmail.com>
2014-02-23 11:41:38 +0200

    Gatherer config file (bare) & course info db

---
+++ gatherer/data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+courses_data = { }
+
+courses_data['ce120'] = { 'code': 'ce120',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE120/'
+                                'site': [ ]
+                                'emails': [ ]
+                                'last_updated' = u''
+                            }
+                            'info': {
+                                'name': 'Προγραμμτισμός Ι'
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE120/'
+                            }
+                        }
+
+courses_data['ce121'] = { 'code': 'ce121',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE121/'
+                                'site': [ ]
+                                'emails': [ ]
+                                'last_updated' = u''
+                            }
+                            'info': {
+                                'name': 'Προγραμμτισμός ΙI'
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE121/'
+                            }
+                        }
+
+courses_data['ce232'] = { 'code': 'ce232',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE232/'
+                                'site': [ ]
+                                'emails': [ ]
+                                'last_updated' = u''
+                            }
+                            'info': {
+                                'name': 'ΟΡΓΑΝΩΣΗ ΚΑΙ ΣΧΕΔΙΑΣΗ Η/Υ'
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE232/'
+                            }
+                        }
+
