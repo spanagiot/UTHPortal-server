036fc3ebb799459a5e6ff70757eea9488be362c8
George TG <george_gougoudis@hotmail.com>
2014-03-29 19:42:37 +0200

    Updated courses in data

--- uthportal/data.py
+++ uthportal/data.py
@@ -42,3 +42,69 @@ courses_data['ce232'] = { 'code': 'ce232',
                             }
                         }
 
+courses_data['ce431'] = { 'code': 'ce431',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE431/',
+                                'site': [ ],
+                                'emails': [ ],
+                                'last_updated': u''
+                            },
+                            'info': {
+                                'name': 'Αρχιτεκτονική Παράλληλων Συστημάτων',
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE431/'
+                            }
+                        }
+
+courses_data['ce536'] = { 'code': 'ce536',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE536/',
+                                'site': [ ],
+                                'emails': [ ],
+                                'last_updated': u''
+                            },
+                            'info': {
+                                'name': 'Εργαστήριο Αναλογικών Συστημάτων VLSI',
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE536/'
+                            }
+                        }
+
+courses_data['ce538'] = { 'code': 'ce538',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE536/',
+                                'site': [ ],
+                                'emails': [ ],
+                                'last_updated': u''
+                            },
+                            'info': {
+                                'name': 'Αρχιτεκτονική Παράλληλων Συστημάτων',
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE538/'
+                            }
+                        }
+
+courses_data['ce230'] = { 'code': 'ce230',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE230/',
+                                'site': [ ],
+                                'emails': [ ],
+                                'last_updated': u''
+                            },
+                            'info': {
+                                'name': 'Ανάλυση Κυκλωμάτων',
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE230/'
+                            }
+                        }
+
+courses_data['ce420'] = { 'code': 'ce420',
+                            'announcements': {
+                                'link':'http://inf-server.inf.uth.gr/courses/CE420/',
+                                'site': [ ],
+                                'emails': [ ],
+                                'last_updated': u''
+                            },
+                            'info': {
+                                'name': 'Σχεδίαση και Ανάπτυξη Λογισμικού',
+                                'link': 'http://inf-server.inf.uth.gr/courses/CE420/',
+                            }
+                        }
+
+
