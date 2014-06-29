83bc0cf97798bd5ff714a50578770e206a54d86d
Konstantinos Kanellis <kkanelli@hotmail.com>
2014-06-29 13:14:16 +0300

    Rewritten test_course.py - Can be used for testing courses parsers

--- uthportal/library/test_course.py
+++ uthportal/library/test_course.py
@@ -1,25 +1,79 @@
-from util import fetch_html, get_bsoup, slugify
-from courses.announcements import parsers
-from os.path import isfile
+from inf import *
+from util import fetch_html, get_bsoup
+from pymongo import MongoClient
+
+code = 'ce121'
+n_tries = 3
+timeout_secs = 10
+
+client = MongoClient()
+db = client.uthportal
 
 if __name__ == '__main__':
     def print_dict():
-        link = 'http://inf-server.inf.uth.gr/courses/CE121/'
-        filename = slugify(link)
+        # Set the query
+        query = {'code': code }
 
-        if not isfile(filename):
-            data = fetch_html(link)
-            with open(filename,'wb') as f:
-                f.write(data)
-        else:
-            with open(filename,'r') as f:
-                data = f.read()
+        # Read from DB link to course
+        try:
+            records = db.inf.courses.find(query)
 
-        list = parsers['ce121'](get_bsoup(data))
+            if records.count() is 0:
+                print '[%s] No entry found' % code
+                return False
 
-        for item in list:
+            if records.count() > 1:
+                print '[%s] Multiple entries found' % code
+
+            link = records[0]['announcements']['link']
+            if link is None:
+                print '[%s] Course does not have "link" field' % code
+                return False
+
+        except Exception as ex:
+            print '[%s] %s' % (code, ex)
+            return False
+
+        print '[%s] Fetching course' % code
+        # Try to fetch_html 'n_tries'
+        for i in xrange(n_tries):
+            html = fetch_html(link, timeout=timeout_secs)
+
+            if html is not None:
+                break
+            elif i is n_tries - 1:
+                return False
+
+        print '[%s] Getting BeautifulSoup object' % code
+        # Get BeautifulSoup Object
+        try:
+            bsoup = get_bsoup(html)
+            if bsoup is None:
+                return False
+        except Exception as ex:
+            print '[%s] %s' % (code, ex)
+            return False
+
+        print '[%s] Trying to parse...' % code
+        # Parse the html and return the data
+        try:
+            parser = globals()[code]
+            data = parser(bsoup)
+        except Exception as ex:
+            print '[%s] %s' % (code, ex)
+            return False
+
+        # Print the data
+        for item in data:
             for key in item:
-                print key + ':' + unicode(item[key])
+                val = item[key]
+                if isinstance(val, basestring):
+                    val = val.decode('utf8')
+                else:
+                    val = unicode(val)
+
+                print '%s: %s' % (key, val)
+            print '\n'
 
     print_dict()
 
